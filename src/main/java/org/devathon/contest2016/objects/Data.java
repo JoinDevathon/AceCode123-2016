5b50f01e06a2741ce081aada3705c187108a2a53
AceCode123 <MrToucan123@gmail.com>
2016-11-05 11:24:59 -0400

    initial commit for BukkitComputer

---
+++ src/main/java/org/devathon/contest2016/objects/Data.java
@@ -0,0 +1,7 @@
+package org.devathon.contest2016.objects;
+
+/**
+ * Created by Anthony on 11/5/2016.
+ */
+public class Data {
+}
---
+++ src/main/java/org/devathon/contest2016/objects/screens/HomeScreen.java
@@ -0,0 +1,7 @@
+package org.devathon.contest2016.objects.screens;
+
+/**
+ * Created by Anthony on 11/5/2016.
+ */
+public class HomeScreen {
+}
